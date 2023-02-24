55cd9e8255afaafa171da7d016294aec02f016ad
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2023-02-24 20:35:33 -0300

    - iniciando controller

--- src/main/java/com/example/controller/FilmeController.java
+++ src/main/java/com/example/controller/FilmeController.java
@@ -1,8 +1,12 @@
 package com.example.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class FilmeController {
@@ -10,6 +14,16 @@ public class FilmeController {
     @GetMapping("/flux")
     public Flux<Integer> flux() {
         return Flux.just(1,2,3);
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        return Mono.just("hello");
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(2)).log();
     }
 
 }
